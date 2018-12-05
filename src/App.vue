9c1b78fe3557a71a17abc90acd3333630b24f89e
Dan Malone <danmalone89@gmail.com>
2018-12-05 14:26:44 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <app-message></app-message>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+import Input from "./components/Input.vue";
+export default {
+  name: "App",
+  components: {
+    appMessage: Message,
+    appInput: Input
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
